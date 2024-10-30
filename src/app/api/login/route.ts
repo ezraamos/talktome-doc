0152ae06af3dbf3d1f1fa3e43b5fd27cd8bf6fd6
azaryahuu <ezra.mollena@lanexcorp.com>
2024-10-30 18:02:52 +0800

    fix: build issues type error

--- src/app/api/login/route.ts
+++
@@ -1,6 +0,0 @@
-// import { auth } from '@/auth';
-
-// export const GET = async (req: Request, res: Response) => {
-//   const session = await auth();
-//   console.log('hello protected route');
-// };
